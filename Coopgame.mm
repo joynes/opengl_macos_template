eec94f43301e5c0149ea9c9891943deb1687e24a
Johannes Kählare <johannes@Johanness-MBP.lan>
2018-03-05 09:51:42 +0100

    Open GL template for macos

--- Coopgame.mm
+++ Coopgame.mm
@@ -5,113 +5,40 @@
 #include <string>
 using namespace std;
 
+#define  LOGI(...)  printf(__VA_ARGS__)
 const unsigned WIDTH = 1024;
 const unsigned HEIGHT = 1024;
 
-void drawFrame();
 void setup();
 
-@interface GameView: NSOpenGLView {
-  CVDisplayLinkRef displayLink;
-}
-@end
-
-@implementation GameView
-- (void)prepareOpenGL {
-  
-  // Synchronize buffer swaps with vertical refresh rate
-  GLint swapInt = 1;
-  [[self openGLContext] setValues:&swapInt forParameter:NSOpenGLCPSwapInterval];
- 
-  // Create a display link capable of being used with all active displays
-  CVDisplayLinkCreateWithActiveCGDisplays(&displayLink);
- 
-  // Set the renderer output callback function
-  CVDisplayLinkSetOutputCallback(displayLink, &MyDisplayLinkCallback, self);
- 
-  // Set the display link for the current renderer
-  CGLContextObj cglContext = [[self openGLContext] CGLContextObj];
-  CGLPixelFormatObj cglPixelFormat = static_cast<CGLPixelFormatObj>([self createPixelFormat]);
-  CVDisplayLinkSetCurrentCGDisplayFromOpenGLContext(displayLink, cglContext, cglPixelFormat);
- 
-  // Activate the display link
-  CVDisplayLinkStart(displayLink);
-  puts("hej");
-  setup();
-}
-
-- (NSOpenGLPixelFormat*)createPixelFormat
-{
-  NSOpenGLPixelFormat *pixelFormat;
-
-  NSOpenGLPixelFormatAttribute attribs[] =
-  {
-    NSOpenGLPFANoRecovery,
-    NSOpenGLPFAAccelerated,
-    NSOpenGLPFADoubleBuffer,
-    NSOpenGLPFAColorSize,        32,
-    NSOpenGLPFAAlphaSize,        8,
-    NSOpenGLPFAScreenMask,
-    CGDisplayIDToOpenGLDisplayMask(kCGDirectMainDisplay),
-    0
-  };
-
-  pixelFormat = [[NSOpenGLPixelFormat alloc] initWithAttributes:attribs];
-  return [pixelFormat autorelease];
-}
-
-static CVReturn MyDisplayLinkCallback(CVDisplayLinkRef displayLink, const CVTimeStamp* now, const CVTimeStamp* outputTime,
-CVOptionFlags flagsIn, CVOptionFlags* flagsOut, void* displayLinkContext)
-{
-    CVReturn result = [(GameView*)displayLinkContext getFrameForTime:outputTime];
-    return result;
-}
- 
-- (CVReturn)getFrameForTime:(const CVTimeStamp*)outputTime
-{
-    // Add your drawing codes here
-  NSOpenGLContext *currentContext = [self openGLContext];
-  [currentContext makeCurrentContext];
-
-  // must lock GL context because display link is threaded
-  CGLLockContext(static_cast<CGLContextObj>([currentContext CGLContextObj]));
-  // Add your drawing codes here
-  drawFrame();
-
-  [currentContext flushBuffer];
-
-  CGLUnlockContext(static_cast<CGLContextObj>([currentContext CGLContextObj]));
-  return kCVReturnSuccess;
- 
-}
- 
-- (void)dealloc
-{
-    // Release the display link
-    CVDisplayLinkRelease(displayLink);
- 
-    [super dealloc];
-}
-@end
-
-int main() {
-  [NSAutoreleasePool new];
-  [NSApplication sharedApplication];
-  [NSApp setActivationPolicy:NSApplicationActivationPolicyRegular];
-
-  id window = [[[NSWindow alloc] initWithContentRect:NSMakeRect(0, 0, WIDTH, HEIGHT) styleMask:NSWindowStyleMaskTitled backing:NSBackingStoreBuffered defer:NO] autorelease];
-  [window setTitle:@"Game"];
-  [window makeKeyAndOrderFront:nil];
-  [NSApp activateIgnoringOtherApps:YES];
-
-  GameView *view = [[GameView alloc] initWithFrame:NSMakeRect(0, 0, WIDTH, HEIGHT)];
-  [view setWantsLayer:YES];
-  [[window contentView] addSubview:view];
-  [NSApp run];
-  puts("test");
-}
-
 static GLuint spriteShader;
+
+static const char vert_shad[] =
+R"(
+attribute vec4 pos;
+void main() {
+  gl_Position = pos;
+}
+)";
+
+static const char frag_shad[] =
+R"(
+void main() {
+  gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+}
+)";
+
+void drawSprites() {
+  glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
+}
+
+void drawFrame() {
+  glViewport(0, 0, (int) WIDTH, HEIGHT);
+  glClearColor(1.0, 0.0, 1.0, 1.0);
+  glClear(GL_COLOR_BUFFER_BIT);
+  drawSprites();
+  glFlush();
+}
 
 static void checkGlError(const char* op) {
   string str_error;
@@ -128,7 +55,6 @@ static void checkGlError(const char* op) {
       NSLog(@"after %s glError %s\n", op, str_error.c_str());
     }
 }
-#define  LOGI(...)  printf(__VA_ARGS__)
 
 GLuint LoadShader(GLenum type, const char *shaderSrc) {
   GLuint shader;
@@ -174,21 +100,6 @@ void checkLinkError(GLint linked, GLuint programId, const char *vertexshader, co
   }
 }
 
-static const char vert_shad[] =
-R"(
-attribute vec4 pos;
-void main() {
-  gl_Position = pos;
-}
-)";
-
-static const char frag_shad[] =
-R"(
-void main() {
-  gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
-}
-)";
-
 GLuint loadProgram(const char *vertexShader, const char *fragmentShader) {
   GLuint vs, fs;
   vs = LoadShader(GL_VERTEX_SHADER, vertexShader);
@@ -207,8 +118,6 @@ GLuint loadProgram(const char *vertexShader, const char *fragmentShader) {
   return programId;
 }
 
-static GLuint vbuffer;
-
 void setup() {
   const char *glsl_header = "";
   const char *vertex_header = glsl_header;
@@ -218,7 +127,7 @@ void setup() {
   const unsigned VERTICES = 4;
   static GLfloat vertices[] = {
     -1.0,  -1.0, // vertex coord
-    0.0,  1.0, // texel coord
+    0.0,  1.0, // texel coord, not used
     1.0,  -1.0,
     1.0,  1.0,
     1.0, 1.0,
@@ -226,6 +135,7 @@ void setup() {
     -1.0, 1.0,
     0.0, 0.0
   };
+  GLuint vbuffer;
   glGenBuffers(1, &vbuffer);
 
   glBindBuffer(GL_ARRAY_BUFFER, vbuffer);
@@ -239,18 +149,100 @@ void setup() {
   glVertexAttribPointer(position, comp, GL_FLOAT, GL_FALSE, (comp + stride)*sizeof(GLfloat), 0);
   glEnableVertexAttribArray(position);
   glUseProgram(spriteShader);
-
 }
 
-void drawSprites() {
-  glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
+@interface GameView: NSOpenGLView {
+  CVDisplayLinkRef displayLink;
 }
+@end
 
-void drawFrame() {
-  glViewport(0, 0, (int) WIDTH, HEIGHT);
-  glClearColor(1.0, 0.0, 1.0, 1.0);
-  glClear(GL_COLOR_BUFFER_BIT);
-  drawSprites();
-  glFlush();
+@implementation GameView
+- (void)prepareOpenGL {
   
+  // Synchronize buffer swaps with vertical refresh rate
+  GLint swapInt = 1;
+  [[self openGLContext] setValues:&swapInt forParameter:NSOpenGLCPSwapInterval];
+
+  // Create a display link capable of being used with all active displays
+  CVDisplayLinkCreateWithActiveCGDisplays(&displayLink);
+
+  // Set the renderer output callback function
+  CVDisplayLinkSetOutputCallback(displayLink, &DisplayLinkCallback, self);
+
+  // Set the display link for the current renderer
+  CGLContextObj cglContext = [[self openGLContext] CGLContextObj];
+  CGLPixelFormatObj cglPixelFormat = static_cast<CGLPixelFormatObj>([self createPixelFormat]);
+  CVDisplayLinkSetCurrentCGDisplayFromOpenGLContext(displayLink, cglContext, cglPixelFormat);
+
+  // Activate the display link
+  CVDisplayLinkStart(displayLink);
+  setup();
+}
+
+- (NSOpenGLPixelFormat*)createPixelFormat
+{
+  NSOpenGLPixelFormat *pixelFormat;
+
+  NSOpenGLPixelFormatAttribute attribs[] =
+  {
+    NSOpenGLPFANoRecovery,
+    NSOpenGLPFAAccelerated,
+    NSOpenGLPFADoubleBuffer,
+    NSOpenGLPFAColorSize,        32,
+    NSOpenGLPFAAlphaSize,        8,
+    NSOpenGLPFAScreenMask,
+    CGDisplayIDToOpenGLDisplayMask(kCGDirectMainDisplay),
+    0
+  };
+
+  pixelFormat = [[NSOpenGLPixelFormat alloc] initWithAttributes:attribs];
+  return [pixelFormat autorelease];
+}
+
+static CVReturn DisplayLinkCallback(CVDisplayLinkRef displayLink, const CVTimeStamp* now, const CVTimeStamp* outputTime,
+CVOptionFlags flagsIn, CVOptionFlags* flagsOut, void* displayLinkContext)
+{
+    CVReturn result = [(GameView*)displayLinkContext getFrameForTime:outputTime];
+    return result;
+}
+
+- (CVReturn)getFrameForTime:(const CVTimeStamp*)outputTime
+{
+    // Add your drawing codes here
+  NSOpenGLContext *currentContext = [self openGLContext];
+  [currentContext makeCurrentContext];
+
+  // must lock GL context because display link is threaded
+  CGLLockContext(static_cast<CGLContextObj>([currentContext CGLContextObj]));
+  // Add your drawing codes here
+  drawFrame();
+
+  [currentContext flushBuffer];
+
+  CGLUnlockContext(static_cast<CGLContextObj>([currentContext CGLContextObj]));
+  return kCVReturnSuccess;
+}
+
+- (void)dealloc
+{
+    // Release the display link
+    CVDisplayLinkRelease(displayLink);
+    [super dealloc];
+}
+@end
+
+int main() {
+  [NSAutoreleasePool new];
+  [NSApplication sharedApplication];
+  [NSApp setActivationPolicy:NSApplicationActivationPolicyRegular];
+
+  id window = [[[NSWindow alloc] initWithContentRect:NSMakeRect(0, 0, WIDTH, HEIGHT) styleMask:NSWindowStyleMaskTitled backing:NSBackingStoreBuffered defer:NO] autorelease];
+  [window setTitle:@"Game"];
+  [window makeKeyAndOrderFront:nil];
+  [NSApp activateIgnoringOtherApps:YES];
+
+  GameView *view = [[GameView alloc] initWithFrame:NSMakeRect(0, 0, WIDTH, HEIGHT)];
+  [view setWantsLayer:YES];
+  [[window contentView] addSubview:view];
+  [NSApp run];
 }
